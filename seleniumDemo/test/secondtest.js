76b70444589d5b4b02f697bfe21b98c28b19d7a2
Vinaud <leyenasd@gmail.com>
2022-04-29 15:38:28 -0300

    add mochawesome to project

--- seleniumDemo/test/secondtest.js
+++ seleniumDemo/test/secondtest.js
@@ -24,6 +24,27 @@ describe("add anoother todo tests", function() {
         await driver.quit();
 
     });
+
+    //second action steps
+    it("Adding a new teast for reporting", async function(){
+
+        let driver = await new Builder().forBrowser("firefox").build();
+
+        await driver.get("http://lambdatest.github.io/sample-todo-app");
+
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+
+        //retrieves the last element content of a list
+        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+             return value;
+        });
+
+        //chai assertion
+        todoText.should.equal("Learn Javascript");
+
+        await driver.quit();
+
+    });
 });
 
 
