80f06bda1943e574d66c29bd4753b9a6cae241ba
Vinaud <leyenasd@gmail.com>
2022-05-02 09:53:30 -0300

    sample cucumber scenario

---
+++ cucumberSample/features/step-definitions/customSteps.js
@@ -0,0 +1,22 @@
+const  {When, Then, Before, After, Given } = require("@cucumber/cucumber");
+
+Given('que eu visito o site {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    
+});
+
+When('eu busco pelo produto {string}', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+   
+});
+
+
+When('eu seleciono um produto', function () {
+    // Write code here that turns the phrase above into concrete actions
+   
+  });
+
+Then('sou direcionado para a página do produto', function () {
+    // Write code here that turns the phrase above into concrete actions
+    
+});
---
+++ cucumberSample/features/test.feature
@@ -0,0 +1,12 @@
+#language: pt
+#encoding: utf-8
+
+Funcionalidade: Busca por produtos
+
+#exemplo de cenário simples
+Cenario: Validar busca por produto
+
+Dado que eu visito o site "http://casasbahia.com.br"
+Quando eu busco pelo produto "panela"
+E eu seleciono um produto
+Entao sou direcionado para a página do produto
