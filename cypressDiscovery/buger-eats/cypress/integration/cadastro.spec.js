dd88973284753b225fe6f01fd0741b57bbe6263a
Vinaud <leyenasd@gmail.com>
2022-05-31 14:20:48 -0300

    Test: Filling fields

--- cypressDiscovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypressDiscovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -4,5 +4,17 @@ describe('Cadastro', () =>{
         cy.visit('https://buger-eats.vercel.app');
         cy.get('a[href="/deliver"]').click();
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+            nome: 'Papito',
+            cpf: '00000014141',
+            email: 'papito@hotmail.com',
+            whatsapp: '11999999999'
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome);
+        cy.get('input[name="cpf"]').type(entregador.cpf);
+        cy.get('input[name="email"]').type(entregador.email);
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
     });
 });
