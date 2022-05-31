b3d77e47f2b784ca131ee5a9d9ae71333662f6a5
Vinaud <leyenasd@gmail.com>
2022-05-31 13:52:36 -0300

    Test: Improve locator

--- cypressDiscovery/buger-eats/cypress/integration/home.spec.js
+++ cypressDiscovery/buger-eats/cypress/integration/home.spec.js
@@ -3,6 +3,6 @@ describe('Home page', () =>{
     it('App deve estar online', () =>{
         cy.viewport(1920, 1080);
         cy.visit('https://buger-eats.vercel.app');
-        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
     });
 });
