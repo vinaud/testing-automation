ff1a12979e60e604e3c35598b078c7aef9a8a86d
Vinaud <leyenasd@gmail.com>
2022-06-01 15:07:04 -0300

    Refactor: implement page object pattern

--- cypressDiscovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypressDiscovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -1,10 +1,8 @@
+import SignupPage from '../Pages/SignupPage';
+
 describe('Cadastro', () =>{
     it('usuário deve se tornar um deliver', () =>{
-        cy.viewport(1920, 1080);
-        cy.visit('https://buger-eats.vercel.app');
-        cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-
+       
         var deliver = {
             name: 'Papito',
             cpf: '00000014141',
@@ -22,37 +20,19 @@ describe('Cadastro', () =>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage();
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
+        signup.go();
+        signup.fillForm(deliver);
+        signup.submit()
 
-        cy.get('input[name="address-number"]').type(deliver.address.number);
-        cy.get('input[name="address-details"]').type(deliver.address.details);
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
+        signup.modalContentShouldBe(expectedMessage);
 
-        cy.get('input[name="address"]').should('have.value', deliver.address.street);
-        cy.get('input[name="district"]').should('have.value', deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state);
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').selectFile(`cypress/fixtures/images/${deliver.cnh}`,{force: true});
-
-        cy.get('form button[type="submit"]').click();
-
-        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
     });
 
     it('CPF incorreto', () =>{
-        cy.viewport(1920, 1080);
-        cy.visit('https://buger-eats.vercel.app');
-        cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-
+       
         var deliver = {
             name: 'Papito',
             cpf: '00000014141AA',
@@ -70,28 +50,13 @@ describe('Cadastro', () =>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage();
+        signup.go();
+        signup.fillForm(deliver);
+        signup.submit();
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
+        const expectedMessage = 'Oops! CPF inválido';
+        signup.alertMessageShouldBe(expectedMessage);
 
-        cy.get('input[name="address-number"]').type(deliver.address.number);
-        cy.get('input[name="address-details"]').type(deliver.address.details);
-
-        cy.get('input[name="address"]').should('have.value', deliver.address.street);
-        cy.get('input[name="district"]').should('have.value', deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state);
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').selectFile(`cypress/fixtures/images/${deliver.cnh}`,{force: true});
-
-        cy.get('form button[type="submit"]').click();
-
-        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido');
-
-       
     });
 });
