1ffd0d8bc2375f684003b3b73833854b77d21236
Vinaud <leyenasd@gmail.com>
2022-06-01 15:39:07 -0300

    Update: Cypress configurations

--- cypressDiscovery/buger-eats/cypress/Pages/SignupPage.js
+++ cypressDiscovery/buger-eats/cypress/Pages/SignupPage.js
@@ -1,8 +1,7 @@
 class SignupPage {
 
     go(){
-        cy.viewport(1920, 1080);
-        cy.visit('https://buger-eats.vercel.app');
+        cy.visit('/');
         cy.get('a[href="/deliver"]').click();
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
     }
