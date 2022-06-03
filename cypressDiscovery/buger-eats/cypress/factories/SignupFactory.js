630acd7a61717a8a97f3a4c46e1cc291c0e27d98
Vinaud <leyenasd@gmail.com>
2022-06-03 14:04:20 -0300

    Update: using 'faker' and 'gerador-validador-cpf' to generate test data

--- cypressDiscovery/buger-eats/cypress/factories/SignupFactory.js
+++ cypressDiscovery/buger-eats/cypress/factories/SignupFactory.js
@@ -1,11 +1,19 @@
+//const { faker } = require('@faker-js/faker');
+var faker = require('faker');
+var cpf = require('gerador-validador-cpf');
+
 export default{
 
     deliver: function() {
 
+        var firstName = faker.name.firstName();
+        var lastName = faker.name.lastName();
+        var fullName = `${firstName} ${lastName}`;
+
         var data = {
-            name: 'Papito',
-            cpf: '00000014141',
-            email: 'papito@hotmail.com',
+            name: fullName,
+            cpf: cpf.generate(),
+            email: faker.internet.email(firstName),
             whatsapp: '11999999999',
             address : {
                 postalcode: '04534011',
