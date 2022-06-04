2363aababee8423b73acf0c239703d7eb1295d23
Vinaud <leyenasd@gmail.com>
2022-06-04 09:43:45 -0300

    Refactor: Using array to check multiple 'it's

--- cypressDiscovery/buger-eats/cypress/integration/signup.spec.js
+++ cypressDiscovery/buger-eats/cypress/integration/signup.spec.js
@@ -51,18 +51,29 @@ describe('Signup', () =>{
 
     });
 
-    it.only('Required fields', function(){
+    context('Required fileds', function(){
 
-        signup.go();
-        signup.submit();
+        const messages = [
+            {field: 'name', output: 'É necessário informar o nome' },
+            {field: 'cpf', output: 'É necessário informar o CPF' },
+            {field: 'email', output: 'É necessário informar o email' },
+            {field: 'postalcode', output: 'É necessário informar o CEP' },
+            {field: 'number', output: 'É necessário informar o número do endereço' },
+            {field: 'delivery_method', output: 'Selecione o método de entrega' },
+            {field: 'cnh', output: 'Adicione uma foto da sua CNH' }   
+        ];
 
-        signup.alertMessageShouldBe('É necessário informar o nome');
-        signup.alertMessageShouldBe('É necessário informar o CPF');
-        signup.alertMessageShouldBe('É necessário informar o email');
-        signup.alertMessageShouldBe('É necessário informar o CEP');
-        signup.alertMessageShouldBe('É necessário informar o número do endereço');
-        signup.alertMessageShouldBe('Selecione o método de entrega');
-        signup.alertMessageShouldBe('Adicione uma foto da sua CNH');
+        before(function(){
+            signup.go();
+            signup.submit();
+        });
 
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                signup.alertMessageShouldBe(msg.output);
+            });
+        })
     });
+
+
 });
