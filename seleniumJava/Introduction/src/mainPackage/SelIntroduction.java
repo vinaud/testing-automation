cc531a1f658e5c2e398b56377e56397a073d0627
Vinaud <leyenasd@gmail.com>
2023-09-13 15:02:04 -0300

    Selenium Hello World java

---
+++ seleniumJava/Introduction/src/mainPackage/SelIntroduction.java
@@ -0,0 +1,15 @@
+package mainPackage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+	//	System.setProperty("webdriver.chrome.driver", path to webdriver)
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		
+		
+	}
+}
