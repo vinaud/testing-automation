8d940c7aaf2eff9b4c32bff96b0c23c0f7353b3e
Vinaud <leyenasd@gmail.com>
2023-09-14 09:15:44 -0300

    java iteration

--- seleniumJava/Introduction/src/javaTutorial/CoreJavaBrushUp1.java
+++ seleniumJava/Introduction/src/javaTutorial/CoreJavaBrushUp1.java
@@ -16,6 +16,16 @@ public class CoreJavaBrushUp1 {
 		arr[0] = 1;
 		
 		int[] arr2 = {1,2,3,4,5};
+		
+		for(int i = 0 ; i < arr2.length; i++) {
+			System.out.println(arr2[i]);
+		}
+		
+		String[] arr3 = {"selenium", "java", "test"};
+		
+		for(String s: arr3) {
+			System.out.println(s);
+		}
 	}
 
 }
