521854076127101cfba87b5d3119d8d07e7da471
Vinaud <leyenasd@gmail.com>
2023-09-18 13:51:55 -0300

    Basic locators

---
+++ seleniumJava/Introduction/src/mainPackage/Locators.java
@@ -0,0 +1,18 @@
+package mainPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+
+	}
+
+}
