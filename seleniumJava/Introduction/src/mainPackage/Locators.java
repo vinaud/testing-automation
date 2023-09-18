ab61422f5e894f33f4af79723a3ef03697de1f5c
Vinaud <leyenasd@gmail.com>
2023-09-18 14:40:31 -0300

    CSS selector

--- seleniumJava/Introduction/src/mainPackage/Locators.java
+++ seleniumJava/Introduction/src/mainPackage/Locators.java
@@ -1,5 +1,7 @@
 package mainPackage;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,10 +10,14 @@ public class Locators {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("Rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 		driver.findElement(By.className("signInBtn")).click();
+		
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 
 	}
 
