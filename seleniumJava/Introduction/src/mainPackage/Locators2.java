0f4fa8ffb888bc4af19dfa1c2cd7d401ddea1804
Vinaud <leyenasd@gmail.com>
2023-09-21 13:06:53 -0300

    Using variables into assertion

--- seleniumJava/Introduction/src/mainPackage/Locators2.java
+++ seleniumJava/Introduction/src/mainPackage/Locators2.java
@@ -13,8 +13,10 @@ public class Locators2 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
+		String name = "rahul";
+		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.id("inputUsername")).sendKeys(name);
 		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.className("signInBtn")).click();
 		
@@ -26,6 +28,7 @@ public class Locators2 {
 		}
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),"Hello " + name + ",");
 		
 		
 	}
