9ac07792ec99c54f5da8bd69c939c3784fb58f73
Vinaud <leyenasd@gmail.com>
2023-09-21 10:52:21 -0300

    New locators

--- seleniumJava/Introduction/src/mainPackage/Locators.java
+++ seleniumJava/Introduction/src/mainPackage/Locators.java
@@ -30,7 +30,13 @@ public class Locators {
 		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@email.com");
 		driver.findElement(By.xpath("//form/input[3]")).sendKeys("123456");
 		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		
 		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.cssSelector("button[class*='submit']")).click();
 	}
-
 }
