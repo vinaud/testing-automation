9330d4aec861594d7c0c6a7298144b44a37619fb
Vinaud <leyenasd@gmail.com>
2023-09-22 15:53:32 -0300

    Parent to child and sibling travesse

---
+++ seleniumJava/Introduction/src/mainPackage/Locators3.java
@@ -0,0 +1,20 @@
+package mainPackage;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).click();
+
+	}
+
+}
