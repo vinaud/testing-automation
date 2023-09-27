6927bf000787d8fbac81528c072d712698e1ec28
Vinaud <leyenasd@gmail.com>
2023-09-27 15:33:06 -0300

    Assertion with testng

--- seleniumJava/Introduction/src/mainPackage/CheckBox.java
+++ seleniumJava/Introduction/src/mainPackage/CheckBox.java
@@ -3,6 +3,7 @@ package mainPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class CheckBox {
 
@@ -10,9 +11,13 @@ public class CheckBox {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
 
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		
+		Assert.assertEquals(driver.findElements(By.cssSelector("input[type='checkbox']")).size(), 6);
 		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
 
 	}
