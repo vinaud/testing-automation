c7af3f07080ed3b7567fd0f4e898053a91d77d98
Vinaud <leyenasd@gmail.com>
2023-09-27 15:16:43 -0300

    Checkbox

---
+++ seleniumJava/Introduction/src/mainPackage/CheckBox.java
@@ -0,0 +1,20 @@
+package mainPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBox {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
+	}
+
+}
